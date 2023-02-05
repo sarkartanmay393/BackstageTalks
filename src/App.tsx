78c51a102460453d6ccb009881d958c0fbf987d8
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-02-05 22:58:05 +0530

    Add: Outsourced Magazine and All Editions

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,65 @@ import LeftSidebar from "./components/LeftSidebar";
 import MagazineBody from "./components/MagazineBody";
 import RightSidebar from "./components/RightSidebar";
 
+const CoverImageIssue6 = require('./assets/cover_issue_6.png');
+const CoverImageIssue5 = require('./assets/cover_issue_5.png');
+const CoverImageIssue4 = require('./assets/cover_issue_4.png');
+const CoverImageIssue3 = require('./assets/cover_issue_3.png');
+const CoverImageIssue2 = require('./assets/cover_issue_2.png');
+const CoverImageIssue1 = require('./assets/cover_issue_1.png');
+
 function App() {
+
+    const magazines = [
+        {
+            name: "Issue #6",
+            buy_link: "https://brot.sk/products/backstage-talks-issue-6?_pos=2&_sid=1c5730cc8&_ss=r",
+            stockAvailable: true,
+            image: CoverImageIssue6,
+        },
+        {
+            name: "Issue #5",
+            buy_link: "https://brot.sk/products/backstage-talks-5?_pos=3&_sid=1c5730cc8&_ss=r",
+            stockAvailable: true,
+            image: CoverImageIssue5,
+        },
+        {
+            name: "Issue #4",
+            buy_link: "",
+            stockAvailable: false,
+            image: CoverImageIssue4,
+        },
+        {
+            name: "Issue #3",
+            buy_link: "https://brot.sk/products/backstage-talks-3?_pos=4&_sid=1c5730cc8&_ss=r",
+            stockAvailable: true,
+            image: CoverImageIssue3,
+        },
+        {
+            name: "Issue #2",
+            buy_link: "https://brot.sk/products/backstage-talks-2?_pos=1&_sid=1c5730cc8&_ss=r",
+            stockAvailable: true,
+            image: CoverImageIssue2,
+        },
+        {
+            name: "Issue #1",
+            buy_link: "",
+            stockAvailable: false,
+            image: CoverImageIssue1,
+        },
+    ];
 
     return (
         <section className={styles.app}>
             <LeftSidebar />
-            <MagazineBody />
+            <section className={styles.contentRow}>
+                <MagazineBody {...magazines[0]}/>
+                <MagazineBody {...magazines[1]}/>
+                <MagazineBody {...magazines[2]}/>
+                <MagazineBody {...magazines[3]}/>
+                <MagazineBody {...magazines[4]}/>
+                <MagazineBody {...magazines[5]}/>
+            </section>
             <RightSidebar />
         </section>
     );
--- src/components/MagazineBody.tsx
+++ src/components/MagazineBody.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import styles from "./MagazineBody.module.css";
 
-const MagazineBody = () => {
-    const issue6url = 'https://brot.sk/products/backstage-talks-issue-6?_pos=2&_sid=1c5730cc8&_ss=r';
+interface magazine {
+    name: string,
+    buy_link: string,
+    stockAvailable: boolean,
+    image: any
+}
 
+const MagazineBody = ({ name, buy_link, stockAvailable, image}: magazine) => {
     return (
         <main className={styles.main}>
             <section className={styles.MainMagazineOverview}>
-                <img className={styles.MagazineCover} src={require('../assets/cover_issue_6.png')} alt='backstage_cover_issue_6' />
-                <h3>Issue #6</h3>
-                <a className={styles.MagazineBuy} href={issue6url}>Buy Here</a>
-                <h4>or in <a className={styles.selectedStores} href='/stores'>selected stores</a>.</h4>
+                <img className={styles.MagazineCover} src={image} alt='backstage_cover_issue_6' />
+                {stockAvailable ?
+                        <>
+                            <h3>{name}</h3>
+                            <a className={styles.MagazineBuy} href={buy_link}>Buy Here</a>
+                        </> : <h3>{name} is sold out!</h3>
+                }
+                {stockAvailable ?
+                    <h4>or in <a className={styles.selectedStores} href='/stores'>selected stores</a>.</h4>
+                    : <h4 className={styles.lastLine}>If you are lucky, you may get the last pieces in <a className={styles.selectedStores} href='/stores'>selected stores</a>.</h4>}
             </section>
         </main>
     );
