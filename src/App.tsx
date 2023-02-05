977d6499e42a654c5d70259c566d1fa6bd2eb007
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-02-05 17:52:16 +0530

    Add: Header and Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
 
+const LogoIcon = require('./assets/logo.png');
 function App() {
   return (
       <>
-      <center>
-          Backstage Talks is coming!
-          <section className='github-footer'>
-              <a href='https://github.com/sarkartanmay393'>Github</a>
-          </section>
-      </center>
-
+          <header className={styles.header}>
+              <img className={styles.headerImage} src={LogoIcon} alt='backstage-logo'/>
+              <h3>info@backstagetalks.com</h3>
+          </header>
+          <main className={styles.main}>
+              <section className={styles.MainMagazineOverview}>
+                  <img className={styles.MagazineCover} src={require('./assets/cover_issue_6.png')} alt='backstage_cover_issue_6' />
+                  <h3>Issue #6</h3>
+                  <button className={styles.MagazineButton}>Buy Here</button>
+                  <h4>or in <a className={styles.selectedStores} href='/stores'>selected stores</a>.</h4>
+              </section>
+          </main>
+          <footer></footer>
       </>
   );
 }
