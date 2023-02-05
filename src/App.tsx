288ba134a8cab633f427ad592a9e5abfe322e55d
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-02-05 18:16:02 +0530

    Fix: First Page ✅

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <>
           <header className={styles.header}>
               <img className={styles.headerImage} src={LogoIcon} alt='backstage-logo'/>
-              <h3>info@backstagetalks.com</h3>
+              <h4>info@backstagetalks.com</h4>
           </header>
           <main className={styles.main}>
               <section className={styles.MainMagazineOverview}>
@@ -17,7 +17,27 @@ function App() {
                   <h4>or in <a className={styles.selectedStores} href='/stores'>selected stores</a>.</h4>
               </section>
           </main>
-          <footer></footer>
+          <footer className={styles.footer}>
+              <section className={styles.FooterLeft}>
+                  <div className={styles.FooterLeftTop}>
+                      <h4 className={styles.FooterText}>
+                          Backstage Talks is a magazine of casual, but in depth dialogues on design and
+                          business. Our decisions shape and influence this complex world—to have a chance
+                          to make the right ones, we need to talk.
+                      </h4>
+                      <p className={styles.CopyrightText}>© 2023 Published by Studio Milk</p>
+                  </div>
+                      <a href='/privacyPolicy' className={styles.PrivacyButton}>Privacy Policy</a>
+              </section>
+              <section className={styles.FooterRight}>
+                  <p>Issue #6</p>
+                  <p>Issue #5</p>
+                  <p>Issue #4</p>
+                  <p>Issue #3</p>
+                  <p>Issue #2</p>
+                  <p>Issue #1</p>
+              </section>
+          </footer>
       </>
   );
 }
